b71f5fce53f7937ebb63b52661691fc4d6836fdb
raldish <pelegrinojayrald@gmail.com>
2024-10-16 10:53:10 +0800

    updated fpdi-fpdf

--- test.php
+++ test.php
@@ -1,16 +1,33 @@
 <?php
-    include('PDF Generator/fpdf/fpdf.php');
-    
-    $pdf = new FPDF();
-    $pdf->SetMargins(40,50,20);
-    $pdf->AddPage();
-    $pdf->SetFont('Arial', 'B', 16);
+// Include FPDF and FPDI
+require_once('PDF Generator/fpdf/fpdf.php');
+require_once('vendor/autoload.php'); // For FPDI
 
+use setasign\Fpdi\Fpdi;
 
-    // Cell($width, $height=0, $text='', $border=0, $ln=0, $align='', $fil=false, $link='') GUIDELINE!
-    // $pdf->SetXY(10, 180);
-    // $pdf->Ln(20);
-    $pdf->Cell(0, 0, "HI PO ATE", 1, 1, "L",false,"");
-    $pdf->Output();
-    
+// Create instance of FPDI
+$pdf = new FPDI();
+
+// Set the source PDF file (in this case, "namefile.pdf")
+$pdf->setSourceFile('PDF Generator/Example-form.pdf');
+
+// Import the first page of the PDF template
+$templateId = $pdf->importPage(1);
+
+// Add a new page using the template
+$pdf->AddPage();
+
+// Use the imported template page
+$pdf->useTemplate($templateId);
+
+// Now overlay dynamic content on top of the template
+$pdf->SetFont('Arial', '', 12);
+$pdf->SetXY(50, 100); // Set position on the page (x, y)
+$pdf->Cell(100, 10, 'Student Name: John Doe');
+
+$pdf->SetXY(90, 120); // Set position for another piece of data
+$pdf->Cell(100, 10, 'Grade: A+');
+
+// Output the new PDF
+$pdf->Output(); // 'D' forces the file to be downloaded
 ?>
