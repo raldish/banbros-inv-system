4e04c5c5017fdf23c0b31b95cdb0b3148a43a087
OJT-jayrald <jayraldpelegrino26@gmail.com>
2024-10-17 23:00:48 +0800

    adjusting design

--- admin/change_password.php
+++ admin/change_password.php
@@ -120,6 +120,14 @@ if (isset($_POST['change_password'])) {
         h3{
             text-align: center;
         }
+        .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        color: white;
+        text-align: center;
+        }
     </style>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -171,3 +179,5 @@ if (isset($_POST['change_password'])) {
             </div>
         </div>
     </div>
+</body>
+</html> 
